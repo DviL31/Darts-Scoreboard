7a256307cbad3a5099385dd371a6b78d1755bbaa
Romain Mouton <rmouton@webedia.fr>
2015-06-15 23:28:42 +0200

    Add new X01Scoreboard scoreboard

---
+++ app/src/main/java/fr/rmouton/dartsscoreboard/models/X01Scoreboard.java
@@ -0,0 +1,10 @@
+package fr.rmouton.dartsscoreboard.models;
+
+/**
+ * A X01 scoreboard. May be 301, 501, 701, 1001.
+ * Created by Romain Mouton on 15/06/15.
+ */
+public class X01Scoreboard {
+
+
+}
